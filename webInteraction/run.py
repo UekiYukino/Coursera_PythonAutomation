92f53f74b299258d49c3d6fab1dfb2046ee72433
Khanh Vu <vtk.khanhvu@gmail.com>
2021-03-05 13:52:56 -0500

    Added comment to run.py for webInteraction activity

--- webInteraction/run.py
+++ webInteraction/run.py
@@ -8,17 +8,23 @@ def extract_data(in_file):
         feedback_list=feedback.readlines()
     feedback_dict={}
     feedback_title=["title","name","date","feedback"]
+    #Iterate through each line of the file and add them to the dictionary with coresponding keys
     for title,content in zip(feedback_title,feedback_list):
         feedback_dict[title]=content.strip()
     return feedback_dict
 
 def process_data(in_dir,url):
+    #Use os.walk to go through the directory and extract information from each file
     for root,_,files in os.walk(in_dir):
         for feedback in files:
             path=os.path.join(root,feedback)
+            #Add information to a key:value pairs dictionary
             feedback_dict=extract_data(path)
-
+            
+            #Make post request to the server with the extracted information
             response=requests.post(url,json=feedback_dict)
+            
+            #Message the user if the file is uploaded successfully or not
             if response.status_code == 201:
                 print("[+] {} uploaded successfully to server".format(path))
             else:
